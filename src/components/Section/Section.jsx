c59510d8b575085b3ef6854f154ff636907c2179
anglsom <anastasiyaladkovskaya@gmail.com>
2022-09-27 21:05:22 +0200

    update

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -11,5 +11,5 @@ export default function Section({ title, children }) {
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.node, 
+  children: PropTypes.node.isRequired, 
 };
